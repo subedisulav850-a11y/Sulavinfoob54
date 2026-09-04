204c1a09af627e829073de676776739cbd1390b0
subedisulav850-a11y <subedisulav850@gmail.com>
2026-09-05 03:29:18 +0545

    Update ReleaseVersion in HTTP headers to OB54

--- lssj.py
+++ lssj.py
@@ -69,7 +69,7 @@ async def Bmw(reg):
     enc = BmwNoiNoiBmvYasYas(G, F, pb)
     url = "https://loginbp.ggpolarbear.com/MajorLogin"
     async with httpx.AsyncClient() as cl:
-        res = await cl.post(url, data=enc, headers={'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 13; CPH2095 Build/RKQ1.211119.001)", 'Connection': "Keep-Alive", 'Accept-Encoding': "gzip", 'Content-Type': "application/octet-stream", 'Expect': "100-continue", 'X-Unity-Version': "2018.4.11f1", 'X-GA': "v1 1", 'ReleaseVersion': "OB53"})
+        res = await cl.post(url, data=enc, headers={'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 13; CPH2095 Build/RKQ1.211119.001)", 'Connection': "Keep-Alive", 'Accept-Encoding': "gzip", 'Content-Type': "application/octet-stream", 'Expect': "100-continue", 'X-Unity-Version': "2018.4.11f1", 'X-GA': "v1 1", 'ReleaseVersion': "OB54"})
         msg = json.loads(json_format.MessageToJson(PoI(res.content, FreeFire_pb2.LoginRes)))
         TOKENS[reg] = {
             'token': f"Bearer {msg.get('token','0')}",
@@ -100,7 +100,7 @@ async def LoL(uid, unk, reg, ep):
     data_enc = BmwNoiNoiBmvYasYas(G, F, payload)
     token, lock, server = await RtY(reg)
     async with httpx.AsyncClient() as cl:
-        res = await cl.post(server+ep, data=data_enc, headers={'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 13; CPH2095 Build/RKQ1.211119.001)", 'Connection': "Keep-Alive", 'Accept-Encoding': "gzip", 'Content-Type': "application/octet-stream", 'Expect': "100-continue", 'Authorization': token, 'X-Unity-Version': "2018.4.11f1", 'X-GA': "v1 1", 'ReleaseVersion': "OB53"})
+        res = await cl.post(server+ep, data=data_enc, headers={'User-Agent': "Dalvik/2.1.0 (Linux; U; Android 13; CPH2095 Build/RKQ1.211119.001)", 'Connection': "Keep-Alive", 'Accept-Encoding': "gzip", 'Content-Type': "application/octet-stream", 'Expect': "100-continue", 'Authorization': token, 'X-Unity-Version': "2018.4.11f1", 'X-GA': "v1 1", 'ReleaseVersion': "OB54"})
         return json.loads(json_format.MessageToJson(PoI(res.content, AccountPersonalShow_pb2.AccountPersonalShowInfo)))
 
 def HeHe(d):
